a8a015e96a9a14e6ffa2a5815ef10af8bb52f9a4
surfshtt <fk8ghtt@gmail.com>
2025-05-05 14:19:04 +0300

    1.1
    final

--- project/project/obj/Debug/net8.0-windows/project.AssemblyInfo.cs
+++ project/project/obj/Debug/net8.0-windows/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30cc925066d71df489ef2f2a1c8ec559dd5bc51")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
